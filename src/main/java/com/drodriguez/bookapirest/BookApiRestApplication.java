fb020012143ce18704de8a80969ac624253b2d54
Diego Rodriguez <diego.rcap@gmail.com>
2020-05-21 21:44:48 -0300

    first commit

---
+++ src/main/java/com/drodriguez/bookapirest/BookApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.drodriguez.bookapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drodriguez/bookapirest/BookApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drodriguez.bookapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
